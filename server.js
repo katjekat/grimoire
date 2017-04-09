e2e39ca92750ab243a22abad4239b453eaa80ff5
System <none>
2017-04-09 20:25:35 +0000

    🔨🐀 Updated with Glitch

---
+++ server.js
@@ -0,0 +1,55 @@
+// server.js
+// where your node app starts
+
+const hypercore = require('hypercore')
+const ram = require('random-access-memory')
+
+const memoryStore = function (file) {
+  return ram()
+}
+let feed = hypercore(memoryStore, {valueEncoding: 'json'})
+
+feed.ready(function () {
+  feed.on('error', function (err) {
+    console.log('error', err)
+  })
+  console.log(`FEED_KEY=${feed.key.toString('hex')}`)
+  console.log(`FEED_ACCESS_KEY=${feed.secretKey.toString('hex')}`)
+})
+
+// init project
+var express = require('express');
+var app = express();
+
+// we've started you off with Express, 
+// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
+
+app.get("/dreams", function (request, response) {
+  response.send(dreams);
+});
+
+// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
+app.post("/dreams", function (request, response) {
+  dreams.push(request.query.dream);
+  response.sendStatus(200);
+});
+
+// Simple in-memory store for now
+var dreams = [
+  "Find and count some sheep",
+  "Climb a really tall mountain",
+  "Wash the dishes"
+];
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
